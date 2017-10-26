4b2755ea2efb592aee641f9cf5df2732c24f722d
Martin Magakian <martin.magakian@gmail.com>
2017-10-26 22:53:18 +0200

    init

--- main.go
+++ main.go
@@ -18,55 +18,6 @@ func stringPtr(s string) *string {
 	return &s
 }
 
-/*func handle(w http.ResponseWriter, r *http.Request) {
-	augRequest := augment.AugmentorRequest{}
-
-	data, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	if err := proto.Unmarshal(data, &augRequest); err != nil {
-		fmt.Println(err)
-	}
-
-	user := augRequest.GetBidRequest().GetUser()
-	fmt.Println("user id:" + user.GetId())
-
-	// unknow user
-	if user.Id == nil {
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-	fmt.Println(user.GetId())
-	rawUserCounter, err := c.Do("INCR", "userId-"+user.GetId())
-	if err != nil {
-		log.Fatalln(err)
-	}
-	userCounter := rawUserCounter.(int64)
-	fmt.Println(userCounter)
-	if userCounter == 1 { // user is unknow
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-
-	// user is know add Augment is segment
-	response := augment.AugmentorResponse{}
-	response.Segments = []*augment.AugmentorResponse_Segment{
-		&augment.AugmentorResponse_Segment{
-			Id:    stringPtr("intentioniste"),
-			Value: stringPtr("Audi A3"),
-		},
-		&augment.AugmentorResponse_Segment{
-			Id:    stringPtr("Intentioniste"),
-			Value: stringPtr("Voiture"),
-		},
-	}
-	respByte, err := proto.Marshal(&response)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	w.Write(respByte)
-}*/
 func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
 	augRequest := augment.AugmentorRequest{}
 	if err := proto.Unmarshal(ctx.PostBody(), &augRequest); err != nil {
@@ -110,11 +61,9 @@ func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
 		log.Fatalln(err)
 	}
 	ctx.SetBody(respByte)
-	//w.Write(respByte)
 }
 
 func main() {
-	//http.HandleFunc("/", handle)
 	connection, err := redis.Dial("tcp", "redis_db:6379")
 	c = connection
 	if err != nil {
@@ -126,6 +75,5 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Println(resp)
-	//http.ListenAndServe(":3000", nil)
 	fasthttp.ListenAndServe(":3000", fastHTTPHandler)
 }
